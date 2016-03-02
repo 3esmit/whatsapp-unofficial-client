1cfc3e7286d3e9eb35b2a490c64446b907f6c5cc
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2016-03-02 13:24:04 -0300

    Update tray.js

--- src/js/tray.js
+++ src/js/tray.js
@@ -1,7 +1,7 @@
 try {
     win.on('minimize', function() {                                              // Get the minimize event
         this.hide();                                                            // Hide window
-        var tray = new gui.Tray({ title: "Skype", icon: 'skype.png' });        // Show tray and create a tray icon
+        var tray = new gui.Tray({ title: "Whatsapp Web", icon: 'whatsapp.png' });        // Show tray and create a tray icon
         tray.on('click', function() {                                         // Show window and remove tray when clicked
         win.show();
         this.remove();                                                   
